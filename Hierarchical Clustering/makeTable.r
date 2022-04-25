d95da16d98ba4e05c46316ab8b8131572c58e804
AnneTorrento <102910249+AnneTorrento@users.noreply.github.com>
2022-04-25 15:56:35 +0300

    Reformats data
    Changes the format of the data into a data frame.
    Column 1 is the cell sample number; Columns 2 to 11 are expression values for mitochondrial genes; Column 12 is the cell type; Column 13 is developmental stage.

---
+++ Hierarchical Clustering/makeTable.r
@@ -0,0 +1,30 @@
+library(Seurat)
+library(dplyr)
+library(data.table)
+library(magrittr)
+library(tibble)
+
+print("READING NORMALIZED ATLAS")
+obj <- readRDS("obj_normalized.rds")
+
+print("READING FEATURES")
+features <- readRDS("features_LongPolyA.rds")
+
+print("SUBSETTING OBJECT")
+obj = obj[features,]
+
+print("CREATING DATA TABLE")
+data <- obj@assays$RNA@data %>% as.matrix %>% t %>% as.data.table
+
+print("CREATING METADATA TABLE")
+celltypes <- obj@meta.data$celltype %>% as.data.table
+stages <- obj@meta.data$stage %>% as.data.table
+md <- cbind(celltypes, stages)
+colnames(md) <- c("celltype", "stage")
+
+print("MERGING")
+df <- cbind(data, md)
+df <- rownames_to_column(df, "sample")
+
+print("SAVING")
+write.table(df, "table.txt")
