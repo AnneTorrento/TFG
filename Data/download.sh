46ba1d702527f792896b447f21f3a2cb354499c0
AnneTorrento <102910249+AnneTorrento@users.noreply.github.com>
2022-05-19 14:11:14 +0300

    script to get the data

---
+++ Data/download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+confirm "Download Atlas data? [y/N]" && curl https://content.cruk.cam.ac.uk/jmlab/atlas_data.tar.gz > atlas_data.tar.gz
+
+# arrayexpress link below:
+# https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6967/E-MTAB-6967.processed.1.zip
